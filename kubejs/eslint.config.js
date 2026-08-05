e5b2daa081bfeab4508a969880a0d5a1b3e2e902
Formic Acid <formic@formic.dev>
2026-08-05 19:50:29 +0200

    probejs/prettier/eslint infra

---
+++ kubejs/eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js"
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_"
+        }
+      ],
+      "no-undef": "off",
+      "no-console": "off",
+      "comma-dangle": ["error", "never"]
+    }
+  }
+]
