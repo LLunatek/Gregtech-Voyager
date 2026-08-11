700e83cb044b0ee95ba7fdcdf9bc599b5e1f232b
bzells <derpers1011@gmail.com>
2026-08-11 15:49:10 -0500

    Add Greg recipe for #58

--- kubejs/server_scripts/recipes/gregify/misc.js
+++ kubejs/server_scripts/recipes/gregify/misc.js
@@ -4,6 +4,7 @@ ServerEvents.recipes((event) => {
 
     event.remove({output: "hangglider:glider_wing"})
     event.remove({output:"hangglider:glider_framework"})
+    event.remove({mod: "comforts"})
     event.remove({output: "#minecraft:wool_carpets", mod: "minecraft", type: "crafting_shaped"})
 
     event.recipes.gtceu.shaped(
@@ -34,6 +35,20 @@ ServerEvents.recipes((event) => {
             C: "#forge:tools/screwdrivers"
         }
     ).id("kjs:glider_framework");
+
+    event.recipes.gtceu.shaped(
+        Item.of("comforts:sleeping_bag_white"),
+        [
+            "   ",
+            "AAA",
+            "BCB"
+        ],
+        {
+            A: "#minecraft:wool_carpets",
+            B: "gtceu:treated_wood_rod",
+            C: "#forge:tools/screwdrivers"
+        }
+    ).id("kjs:sleeping_bag");
 
 
     event.recipes.gtceu.assembler("kubejs:framed_cube").itemInputs("4x gtceu:wood_screw", "4x minecraft:stick").itemOutputs("16x framedblocks:framed_cube").duration(20).EUt(2)
