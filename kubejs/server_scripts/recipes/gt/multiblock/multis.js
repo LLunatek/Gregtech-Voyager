ccf751f2d9e177feda8db9e675893f612f2acf48
bzells <derpers1011@gmail.com>
2026-08-11 14:16:07 -0500

    Refactor

---
+++ kubejs/server_scripts/recipes/gt/multiblock/multis.js
@@ -0,0 +1,17 @@
+ServerEvents.recipes((event) => {
+    // @ts-ignore
+    event.shaped(
+        Item.of("gtceu:radiation_chamber", 1), // arg 1: output
+        [
+            "ADA",
+            "CBC", // arg 2: the shape (array of strings)
+            "ADA"
+        ],
+        {
+            A: "gtceu:titanex-594-hta_rotor",
+            B: "gtceu:iv_fluid_heater", //arg 3: the mapping object
+            C: "gtceu:ostrum_double_cable",
+            D: "#gtceu:circuits/luv"
+        }
+    )
+})
--- kubejs/server_scripts/recipes/gt_removal/shaped.js
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-ServerEvents.recipes((event) => {
-    event.remove({ type: "minecraft:crafting_shaped", output: "gtceu:steel_plate" })
-    event.remove({ type: "minecraft:crafting_shaped", output: "gtceu:brass_dust" })
-    event.remove({ output: "minecraft:bricks" })
-})
